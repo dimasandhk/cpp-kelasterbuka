c9e40666b7492e151d650669aa7d47f25aec442b
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-12-08 20:21:11 +0700

    51. Operasi pada string

--- string/pengenalan.cpp
+++ string/pengenalan.cpp
@@ -3,12 +3,21 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
   char p = 's';
-  string str = "S";
+  string str = "Sandhika Galih";
+    
+  cout << str << endl << endl;
+  string arr[3] = {"dimas andhika", "galih pratama", "sandhika galih"};
+  for(string nama : arr) {
+    if (nama.compare("galih") > 0) {
+      cout << "ketemu" << endl;
+    } else {
+      cout << nama << endl;
+    }
+  }
+
+  return 0;
+}
+
   // Char = 1 huruf tanda kutip 1  
   // String = bebas tanda kutip 2
   // Char bukan string tapi string pasti kumpulan char
-  
-
-  cout << str;
-  return 0;
-}
