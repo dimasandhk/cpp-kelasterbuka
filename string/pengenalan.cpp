1cd52edb172532c059c5b71357e7a9869e1fd945
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-12-08 20:08:44 +0700

    50. Pengenalan String

---
+++ string/pengenalan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  char p = 's';
+  string str = "S";
+  // Char = 1 huruf tanda kutip 1  
+  // String = bebas tanda kutip 2
+  // Char bukan string tapi string pasti kumpulan char
+  
+
+  cout << str;
+  return 0;
+}
