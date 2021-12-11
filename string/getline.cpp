249b85bb81783bb8caa6e6eb886a5ec30f3cc4df
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-12-11 11:27:28 +0700

    55. Lat Getline

--- string/getline.cpp
+++ string/getline.cpp
@@ -4,8 +4,18 @@ using namespace std;
 int main(int argc, char const *argv[]) {
   string input;
   cout << "Input: ";
+  // Getline bisa nangkap kalimat
   getline(cin, input);
   
-  cout << input << endl;
+  int posisi = 0;
+  int jumlah = 0;
+
+  while(true) {
+    posisi = input.find(" ", posisi + 1);
+    jumlah++;
+    if (posisi < 0) break;
+  }  
+
+  cout << "Jumlah kata: " << jumlah << endl;
   return 0;
 }
